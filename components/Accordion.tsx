10df8f1e7447ab6204a856d6c63be2e38ee7b60f
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2021-01-19 15:05:50 -0300

    Accordion component finished and sidebar created

---
+++ components/Accordion.tsx
@@ -0,0 +1,49 @@
+import styles from '../styles/components/Accordion.module.css'
+import { useState, useRef } from 'react'
+
+import { FiChevronRight, FiRotateCcw } from 'react-icons/fi'
+
+interface AccordionProps {
+  title: string,
+  children: React.ReactNode
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, children, className }) => {
+  const [activeAccordion, setActiveAccordion] = useState('')
+  const [accordionContentHeight, setAccordionContentHeight] = useState('0px')
+
+  const contentRef = useRef(null)
+
+  function toggleAccordion() {
+    setActiveAccordion(oldValue => oldValue === '' ? 'active' : '')
+    setAccordionContentHeight(activeAccordion === 'active' ? '0px' : `${contentRef.current.scrollHeight}px`)
+  }
+
+  return (
+    <div className={styles.accordionContainer}>
+      <button
+        className={`${styles.accordion} ${activeAccordion}`}
+        onClick={toggleAccordion}
+      >
+        <FiChevronRight
+          style={{
+            transform: activeAccordion === 'active' ? 'rotate(90deg)' : 'none'
+          }}
+          size={20} color="white"
+        />
+        <p className={styles.accordionTitle}>
+          {title}
+        </p>
+      </button>
+      <div
+        className={styles.accordionContent}
+        ref={contentRef}
+        style={{ maxHeight: accordionContentHeight }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Accordion
--- pages/app.tsx
+++ pages/app.tsx
@@ -1,16 +1,39 @@
 import Head from 'next/head'
+import styles from '../styles/pages/app.module.css'
+
+import { FiInbox, FiCalendar, FiFolder } from 'react-icons/fi'
+
+import Accordion from '../components/Accordion'
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Anthe Todo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>App Page!</h1>
-      </main>
+      <aside className={styles.sideBar}>
+        <nav className={styles.navigationItems}>
+          <button>
+            <FiCalendar size={20} />
+            Hoje
+          </button>
+          <button>
+            <FiInbox size={20} />
+            Inbox
+          </button>
+          <Accordion
+            title="Projetos"
+          >
+            <p>hello1</p>
+            <p>hello1</p>
+            <p>hello1</p>
+          </Accordion>
+        </nav>
+      </aside>
+
+      <main className={styles.main}></main>
     </div>
   )
 }
